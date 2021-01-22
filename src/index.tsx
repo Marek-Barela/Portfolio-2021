5ac3e05334a75e824141ae754aaa3d0f108dde3f
Marek Bareła <marek.barela@wp.pl>
2021-01-22 19:20:57 +0100

    aplication wrapped by router also types for react-router installed

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Root from "./root";
 import App from "./components/App";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 
 ReactDOM.render(
   <Root>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Root>,
   document.getElementById("root")
 );
