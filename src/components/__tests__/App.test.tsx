5b7925ca08c065f22e93aac58a4f15f3767fb958
Marek Bareła <marek.barela@wp.pl>
2021-01-23 07:59:29 +0100

    App component test commited

---
+++ src/components/__tests__/App.test.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { shallow } from "enzyme";
+import App from "../App";
+import LandingPage from "../pages/LandingPage";
+import AboutPage from "../pages/AboutPage";
+import WorksPage from "../pages/WorksPage";
+import ContactPage from "../pages/ContactPage";
+
+let wrapped = null;
+
+beforeEach(() => {
+  wrapped = shallow(<App />);
+});
+
+describe("Shows all component pages in App component", () => {
+  it("Show one LandingPage component", () => {
+    expect(wrapped.find(LandingPage).length).toEqual(1);
+  });
+
+  it("Show one AboutPage component", () => {
+    expect(wrapped.find(AboutPage).length).toEqual(1);
+  });
+
+  it("Show one WorksPage component", () => {
+    expect(wrapped.find(WorksPage).length).toEqual(1);
+  });
+
+  it("Show one ContactPage component", () => {
+    expect(wrapped.find(ContactPage).length).toEqual(1);
+  });
+});
