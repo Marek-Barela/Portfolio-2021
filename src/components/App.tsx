9c76ef7036c543d0f9aeb217af06cb3e4b50576f
Marek Bareła <marek.barela@wp.pl>
2021-01-23 07:44:52 +0100

    basic app structure commited

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import AboutPage from "./pages/AboutPage";
+import WorksPage from "./pages/WorksPage";
+import ContactPage from "./pages/ContactPage";
 
 const App = () => {
-  return <div>App Component</div>;
+  return (
+    <Switch>
+      <Route exact path="/">
+        <LandingPage />
+      </Route>
+      <Route path="/about">
+        <AboutPage />
+      </Route>
+      <Route path="/works">
+        <WorksPage />
+      </Route>
+      <Route path="/contact">
+        <ContactPage />
+      </Route>
+    </Switch>
+  );
 };
 
 export default App;
