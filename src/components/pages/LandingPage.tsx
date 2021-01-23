f6c51529e1721619eb06feb4a1e126c83c1ebe35
Marek Bareła <marek.barela@wp.pl>
2021-01-23 10:10:29 +0100

    LandingLinks component created fully styled and connected with LandingPage component

--- src/components/pages/LandingPage.tsx
+++ src/components/pages/LandingPage.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import LandingContainer from "../LandingContainer";
+import LandingLinks from "../LandingLinks";
 
 const LandingPage = () => {
-  return <LandingContainer />;
+  return (
+    <LandingContainer>
+      <LandingLinks />
+    </LandingContainer>
+  );
 };
 
 export default LandingPage;
