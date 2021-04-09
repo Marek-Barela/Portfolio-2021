ecb03a2f35555466683a0c0f18017e1d8294bfa9
Marek Bareła <marek.barela@wp.pl>
2021-04-09 21:01:25 +0200

    projects list update

--- src/lang/english.tsx
+++ src/lang/english.tsx
@@ -1,151 +1,161 @@
-import {
-  Menu,
-  AboutContent,
-  WorksContent,
-  ContactContent,
-} from "./languageInterface";
+import { Menu, AboutContent, WorksContent, ContactContent } from './languageInterface';
 
-import simpler from "../assets/images/simpler.png";
-import raccoon from "../assets/images/raccoon.png";
-import mailbox from "../assets/images/mailbox.png";
-import currencyConverter from "../assets/images/currencyconverter.png";
-import slick from "../assets/images/slick.png";
+import marketHub from '../assets/images/markethub.png';
+import simpler from '../assets/images/simpler.png';
+import raccoon from '../assets/images/raccoon.png';
+import mailbox from '../assets/images/mailbox.png';
+import currencyConverter from '../assets/images/currencyconverter.png';
+import slick from '../assets/images/slick.png';
 
 export const menu: Menu = {
-  hello: "Hello.",
-  Iam: "I am",
-  name: "Marek",
-  about: "About",
-  works: "Works",
-  contact: "Contact",
+	hello: 'Hello.',
+	Iam: 'I am',
+	name: 'Marek',
+	about: 'About',
+	works: 'Works',
+	contact: 'Contact',
 };
 
 export const aboutContent: AboutContent = {
-  header: "I project & build digital products",
-  about: "About",
-  content: [
-    "Hey my name is Marek. I'm a web developer with a good understanding of front-end technologies focused on producing clean and maintainable code. I'm looking for opportunities to contribute my skills and collaborate with a group to create amazing products.",
-    "I feel very comfortable in React technology, I now how to create a huge number of functionalities, paying attention to the performance and architecture of a given solution. I follow the news all the time, so I don't lag behind when a new update comes out.",
-    "I only create practical projects, using the latest technological solutions, not only front-end, but also back-end.",
-    "The technologies I mainly use are React, Redux, Saga, TypeScript, Node.js, Express",
-  ],
+	header: 'I project & build digital products',
+	about: 'About',
+	content: [
+		"Hey my name is Marek. I'm a web developer with a good understanding of front-end technologies focused on producing clean and maintainable code. I'm looking for opportunities to contribute my skills and collaborate with a group to create amazing products.",
+		"I feel very comfortable in React technology, I now how to create a huge number of functionalities, paying attention to the performance and architecture of a given solution. I follow the news all the time, so I don't lag behind when a new update comes out.",
+		'I only create practical projects, using the latest technological solutions, not only front-end, but also back-end.',
+		'The technologies I mainly use are React, Redux, Saga, TypeScript, Node.js, Express',
+	],
 };
 
 export const worksContent: WorksContent = {
-  projects: [
-    {
-      id: "1",
-      projectTitle: "Simpler",
-      projectDescription: "Practical application for managing tasks",
-      link: "/works/simpler",
-      content: [
-        "Complex full stack application to manage projects and tasks. Application is based on popular application Todoist",
-        "Application was created with React, Redux, Redux-Saga, Typescript, Node.js, Express and MongoDB",
-      ],
-      image: simpler,
-      externalLinks: [
-        { title: "Github", link: "https://github.com/Marek-Barela/Simpler" },
-        { title: "Simpler", link: "https://simpler.herokuapp.com/" },
-      ],
-    },
-    {
-      id: "2",
-      projectTitle: "Raccon Notes",
-      projectDescription:
-        "An application designed to store and organize your notes",
-      link: "/works/racconnotes",
-      content: [
-        "Raccoon is a great application for managing notes with text editor functionality.",
-        "Application was created as full stack application with technologies such as Node.js Express and MongoDB.",
-      ],
-      image: raccoon,
-      externalLinks: [
-        {
-          title: "Github",
-          link: "https://github.com/Marek-Barela/Raccoon-notes",
-        },
-        { title: "Raccon Notes", link: "https://raccoon-notes.herokuapp.com/" },
-      ],
-    },
-    {
-      id: "3",
-      projectTitle: "Mailbox",
-      projectDescription:
-        "Complex client side functionality of email application",
-      link: "/works/mailbox",
-      content: [
-        "The application created to resemble of email functionality. It contain rich amount of features such as sorting, moving messages, new folders creating, marking messages as read and unread etc.",
-        "Mailbox was created only as a frontend application, therefore it does not contain an extensive backend. All changes in the app return to their default settings after refreshing.",
-      ],
-      image: mailbox,
-      externalLinks: [
-        {
-          title: "Github",
-          link: "https://github.com/Marek-Barela/Mailbox",
-        },
-        {
-          title: "Mailbox",
-          link: "https://agitated-nobel-aff5f8.netlify.app/",
-        },
-      ],
-    },
-    {
-      id: "4",
-      projectTitle: "Currency Converter",
-      projectDescription:
-        "Currency conversion and historical data for 33 different currencies.",
-      link: "/works/currency-converter",
-      content: [
-        "Application created as trial for very extended project setup",
-        "The converter contains a very rich and scalable project structure with the use of many libraries",
-        "The aim of the project was to create an application with a persist state after reloading, conversion for specific currency and historical rates with graph",
-        "Used technology: React, Redux, Redux-Saga, Typescript, Typesafe-Actions, CSS Modules, Ramda, Axios",
-      ],
-      image: currencyConverter,
-      externalLinks: [
-        {
-          title: "Github",
-          link: "https://github.com/Marek-Barela/Currency-Converter",
-        },
-        {
-          title: "Currency Converter",
-          link: "https://hardcore-leakey-952fc7.netlify.com/",
-        },
-      ],
-    },
-    {
-      id: "5",
-      projectTitle: "Slick",
-      projectDescription: "Handcrafted web template.",
-      link: "/works/slick",
-      content: ["Beautiful web template creted in react"],
-      image: slick,
-      externalLinks: [
-        {
-          title: "Github",
-          link: "https://github.com/Marek-Barela/Slick-React",
-        },
-        {
-          title: "Slick",
-          link: "https://determined-mccarthy-d890e0.netlify.app/",
-        },
-      ],
-    },
-  ],
+	projects: [
+		{
+			id: '1',
+			projectTitle: 'MarketHub',
+			projectDescription:
+				'An application for checking the prices of rates and stock charts of various companies',
+			link: '/works/markethub',
+			content: [
+				'An extensive application for checking stock prices from around the world. The application is based on the Alpha Vantage API',
+				'Application was created with: React, Redux, Redux-Saga, Redux-persist, Reselect, Typescript, Apexchart',
+			],
+			image: marketHub,
+			externalLinks: [
+				{ title: 'Github', link: 'https://github.com/Marek-Barela/MarketHub' },
+				{ title: 'MarketHub', link: 'https://markethub-pro.netlify.app/' },
+			],
+		},
+		{
+			id: '2',
+			projectTitle: 'Simpler',
+			projectDescription: 'Practical application for managing tasks',
+			link: '/works/simpler',
+			content: [
+				'Complex full stack application to manage projects and tasks. Application is based on popular application Todoist',
+				'Application was created with: React, Redux, Redux-Saga, Typescript, Node.js, Express and MongoDB',
+			],
+			image: simpler,
+			externalLinks: [
+				{ title: 'Github', link: 'https://github.com/Marek-Barela/Simpler' },
+				{ title: 'Simpler', link: 'https://simpler.herokuapp.com/' },
+			],
+		},
+		{
+			id: '3',
+			projectTitle: 'Raccon Notes',
+			projectDescription: 'An application designed to store and organize your notes',
+			link: '/works/racconnotes',
+			content: [
+				'Raccoon is a great application for managing notes with text editor functionality.',
+				'Application was created as full stack application with technologies such as Node.js Express and MongoDB.',
+			],
+			image: raccoon,
+			externalLinks: [
+				{
+					title: 'Github',
+					link: 'https://github.com/Marek-Barela/Raccoon-notes',
+				},
+				{ title: 'Raccon Notes', link: 'https://raccoon-notes.herokuapp.com/' },
+			],
+		},
+		{
+			id: '4',
+			projectTitle: 'Mailbox',
+			projectDescription: 'Complex client side functionality of email application',
+			link: '/works/mailbox',
+			content: [
+				'The application created to resemble of email functionality. It contain rich amount of features such as sorting, moving messages, new folders creating, marking messages as read and unread etc.',
+				'Mailbox was created only as a frontend application, therefore it does not contain an extensive backend. All changes in the app return to their default settings after refreshing.',
+			],
+			image: mailbox,
+			externalLinks: [
+				{
+					title: 'Github',
+					link: 'https://github.com/Marek-Barela/Mailbox',
+				},
+				{
+					title: 'Mailbox',
+					link: 'https://agitated-nobel-aff5f8.netlify.app/',
+				},
+			],
+		},
+		{
+			id: '5',
+			projectTitle: 'Currency Converter',
+			projectDescription:
+				'Currency conversion and historical data for 33 different currencies.',
+			link: '/works/currency-converter',
+			content: [
+				'Application created as trial for very extended project setup',
+				'The converter contains a very rich and scalable project structure with the use of many libraries',
+				'The aim of the project was to create an application with a persist state after reloading, conversion for specific currency and historical rates with graph',
+				'Used technology: React, Redux, Redux-Saga, Typescript, Typesafe-Actions, CSS Modules, Ramda, Axios',
+			],
+			image: currencyConverter,
+			externalLinks: [
+				{
+					title: 'Github',
+					link: 'https://github.com/Marek-Barela/Currency-Converter',
+				},
+				{
+					title: 'Currency Converter',
+					link: 'https://hardcore-leakey-952fc7.netlify.com/',
+				},
+			],
+		},
+		{
+			id: '6',
+			projectTitle: 'Slick',
+			projectDescription: 'Handcrafted web template.',
+			link: '/works/slick',
+			content: ['Beautiful web template creted in react'],
+			image: slick,
+			externalLinks: [
+				{
+					title: 'Github',
+					link: 'https://github.com/Marek-Barela/Slick-React',
+				},
+				{
+					title: 'Slick',
+					link: 'https://determined-mccarthy-d890e0.netlify.app/',
+				},
+			],
+		},
+	],
 };
 
 export const contactContent: ContactContent = {
-  header: "Let's make something great!",
-  contact: "Contact",
-  content: [
-    "If you like my projects and would like to work with an open-minded assertive person, Feel free to reach out through any platforms bellow.",
-  ],
-  externalLinks: [
-    { title: "marek.barela@wp.pl", link: "mailto:marek.barela@wp.pl" },
-    { title: "Github", link: "https://github.com/Marek-Barela" },
-    {
-      title: "Linkedin",
-      link: "https://www.linkedin.com/in/marek-bare%C5%82a/",
-    },
-  ],
+	header: "Let's make something great!",
+	contact: 'Contact',
+	content: [
+		'If you like my projects and would like to work with an open-minded assertive person, Feel free to reach out through any platforms bellow.',
+	],
+	externalLinks: [
+		{ title: 'marek.barela@wp.pl', link: 'mailto:marek.barela@wp.pl' },
+		{ title: 'Github', link: 'https://github.com/Marek-Barela' },
+		{
+			title: 'Linkedin',
+			link: 'https://www.linkedin.com/in/marek-bare%C5%82a/',
+		},
+	],
 };
--- src/lang/polish.tsx
+++ src/lang/polish.tsx
@@ -1,152 +1,163 @@
-import {
-  Menu,
-  AboutContent,
-  WorksContent,
-  ContactContent,
-} from "./languageInterface";
+import { Menu, AboutContent, WorksContent, ContactContent } from './languageInterface';
 
-import simpler from "../assets/images/simpler.png";
-import raccoon from "../assets/images/raccoon.png";
-import mailbox from "../assets/images/mailbox.png";
-import currencyConverter from "../assets/images/currencyconverter.png";
-import slick from "../assets/images/slick.png";
+import marketHub from '../assets/images/markethub.png';
+import simpler from '../assets/images/simpler.png';
+import raccoon from '../assets/images/raccoon.png';
+import mailbox from '../assets/images/mailbox.png';
+import currencyConverter from '../assets/images/currencyconverter.png';
+import slick from '../assets/images/slick.png';
 
 export const menu: Menu = {
-  hello: "Witaj.",
-  Iam: "Jestem",
-  name: "Marek",
-  about: "O mnie",
-  works: "Prace",
-  contact: "Kontakt",
+	hello: 'Witaj.',
+	Iam: 'Jestem',
+	name: 'Marek',
+	about: 'O mnie',
+	works: 'Prace',
+	contact: 'Kontakt',
 };
 
 export const aboutContent: AboutContent = {
-  header: "Projektuje i buduje produkty cyfrowe",
-  about: "O mnie",
-  content: [
-    "Cześć, nazywam się Marek. Jestem osobą, która dobrze rozumie technologie front-endowe. Skupiam się na tworzeniu czystego i łatwego do utrzymania kodu. Szukam okazji, aby wnieść swój wkład w rozwój projektów i mieć możliwość współpracy w fajnym zespole, aby tworzyć niesamowite produkty.",
-    "Bardzo swobodnie czuję się w technologii React, potrafię stworzyć ogromną liczbę funkcjonalności, zwracając przy tym dużą uwagę na wydajność oraz architekturę danego rozwiązania. Śledzę cały czas nowinki, dzięki czemu nie pozostaje w tyle, gdy wychodzi nowa aktualizacja.",
-    "Tworzę tylko praktyczne aplikacje, wykorzystując przy tym najnowsze rozwiązania technologiczne nie tylko front-endowe, ale i back-endowe.",
-    "Technologie, których głównie używam to React, Redux, Saga, TypeScript, Node.js, Express",
-  ],
+	header: 'Projektuje i buduje produkty cyfrowe',
+	about: 'O mnie',
+	content: [
+		'Cześć, nazywam się Marek. Jestem osobą, która dobrze rozumie technologie front-endowe. Skupiam się na tworzeniu czystego i łatwego do utrzymania kodu. Szukam okazji, aby wnieść swój wkład w rozwój projektów i mieć możliwość współpracy w fajnym zespole, aby tworzyć niesamowite produkty.',
+		'Bardzo swobodnie czuję się w technologii React, potrafię stworzyć ogromną liczbę funkcjonalności, zwracając przy tym dużą uwagę na wydajność oraz architekturę danego rozwiązania. Śledzę cały czas nowinki, dzięki czemu nie pozostaje w tyle, gdy wychodzi nowa aktualizacja.',
+		'Tworzę tylko praktyczne aplikacje, wykorzystując przy tym najnowsze rozwiązania technologiczne nie tylko front-endowe, ale i back-endowe.',
+		'Technologie, których głównie używam to React, Redux, Saga, TypeScript, Node.js, Express',
+	],
 };
 
 export const worksContent: WorksContent = {
-  projects: [
-    {
-      id: "1",
-      projectTitle: "Simpler",
-      projectDescription:
-        "Aplikacja do zarządzania zadaniami do wykonania i organizacją czasu.",
-      link: "/works/simpler",
-      content: [
-        "Złożona aplikacja do zarządzania projektami i zadaniami. Aplikacja oparta jest na popularnej aplikacji Todoist",
-        "Wykorzystane technologie React, Redux, Redux-Saga, Typescript, Node.js, Express i MongoDB.",
-      ],
-      image: simpler,
-      externalLinks: [
-        { title: "Github", link: "https://github.com/Marek-Barela/Simpler" },
-        { title: "Simpler", link: "https://simpler.herokuapp.com/" },
-      ],
-    },
-    {
-      id: "2",
-      projectTitle: "Raccon Notes",
-      projectDescription:
-        "Aplikacja zaprojektowana do przechowywania i organizowania notatek",
-      link: "/works/racconnotes",
-      content: [
-        "Raccoon to świetna aplikacja do zarządzania notatkami z funkcjonalnością edytora tekstu",
-        "Aplikacja została stworzona przy użycu technologii takich jak Node.js Express i MongoDB.",
-      ],
-      image: raccoon,
-      externalLinks: [
-        {
-          title: "Github",
-          link: "https://github.com/Marek-Barela/Raccoon-notes",
-        },
-        { title: "Raccon Notes", link: "https://raccoon-notes.herokuapp.com/" },
-      ],
-    },
-    {
-      id: "3",
-      projectTitle: "Mailbox",
-      projectDescription:
-        "Rozbudowana funkcjonalność aplikacji pocztowej po stronie klienta",
-      link: "/works/mailbox",
-      content: [
-        "Aplikacja stworzona na podobieństwo poczty elektronicznej posiada bogatą ilość operacji takie jak sortowanie, przenoszenie zaznaczonych wiadomości, tworzenie nowych folderów, oznaczanie wiadomości jako przeczytane i nieprzeczytane itd.",
-        "Mailbox został stworzony tylko jako aplikacja frontendowa, przez co nie posiada rozbudowanego backendu. Wszystkie zmiany w aplikacji wracają do ustawień domyślnych po odświeżeniu.",
-      ],
-      image: mailbox,
-      externalLinks: [
-        {
-          title: "Github",
-          link: "https://github.com/Marek-Barela/Mailbox",
-        },
-        {
-          title: "Mailbox",
-          link: "https://agitated-nobel-aff5f8.netlify.app/",
-        },
-      ],
-    },
-    {
-      id: "4",
-      projectTitle: "Currency Converter",
-      projectDescription:
-        "Przeliczanie i dane historyczne dla 33 różnych walut.",
-      link: "/works/currency-converter",
-      content: [
-        "Aplikacja stworzona jako wersja próbna dla bardzo rozbudowanej konfiguracji projektu",
-        "Konwerter zawiera bardzo bogatą i skalowalną strukturę projektu z wykorzystaniem wielu bibliotek.",
-        "Celem projektu było stworzenie aplikacji z zachowaniem danych po odświerzeniu, możliwość sprawdzenia cen dla określonej waluty oraz kursów historycznych z wykresem.",
-        "Użyte technologie: React, Redux, Redux-Saga, Typescript, Typesafe-Actions, CSS Modules, Ramda, Axios",
-      ],
-      image: currencyConverter,
-      externalLinks: [
-        {
-          title: "Github",
-          link: "https://github.com/Marek-Barela/Currency-Converter",
-        },
-        {
-          title: "Currency Converter",
-          link: "https://hardcore-leakey-952fc7.netlify.com/",
-        },
-      ],
-    },
-    {
-      id: "5",
-      projectTitle: "Slick",
-      projectDescription: "Szablon aplikacji webowej",
-      link: "/works/slick",
-      content: ["Aplikacja z bardzo ładnym designem stworzona w Reacie."],
-      image: slick,
-      externalLinks: [
-        {
-          title: "Github",
-          link: "https://github.com/Marek-Barela/Slick-React",
-        },
-        {
-          title: "Slick",
-          link: "https://determined-mccarthy-d890e0.netlify.app/",
-        },
-      ],
-    },
-  ],
+	projects: [
+		{
+			id: '1',
+			projectTitle: 'MarketHub',
+			projectDescription:
+				'Aplikacja do sprawdzania kursów i wykresów giełdowych różnych firm',
+			link: '/works/markethub',
+			content: [
+				'Rozbudowana aplikacja do sprawdzania kursów akcji z całego świata. Aplikacja oparta jest na Alpha Vantage API',
+				'Wykorzystane technologie React, Redux, Redux-Saga, Redux-persist, Reselect, Typescript, Apexchart',
+			],
+			image: marketHub,
+			externalLinks: [
+				{ title: 'Github', link: 'https://github.com/Marek-Barela/MarketHub' },
+				{ title: 'MarketHub', link: 'https://markethub-pro.netlify.app/' },
+			],
+		},
+		{
+			id: '2',
+			projectTitle: 'Simpler',
+			projectDescription:
+				'Aplikacja do zarządzania zadaniami do wykonania i organizacją czasu.',
+			link: '/works/simpler',
+			content: [
+				'Złożona aplikacja do zarządzania projektami i zadaniami. Aplikacja oparta jest na popularnej aplikacji Todoist',
+				'Wykorzystane technologie React, Redux, Redux-Saga, Typescript, Node.js, Express i MongoDB.',
+			],
+			image: simpler,
+			externalLinks: [
+				{ title: 'Github', link: 'https://github.com/Marek-Barela/Simpler' },
+				{ title: 'Simpler', link: 'https://simpler.herokuapp.com/' },
+			],
+		},
+		{
+			id: '3',
+			projectTitle: 'Raccon Notes',
+			projectDescription:
+				'Aplikacja zaprojektowana do przechowywania i organizowania notatek',
+			link: '/works/racconnotes',
+			content: [
+				'Raccoon to świetna aplikacja do zarządzania notatkami z funkcjonalnością edytora tekstu',
+				'Aplikacja została stworzona przy użycu technologii takich jak Node.js Express i MongoDB.',
+			],
+			image: raccoon,
+			externalLinks: [
+				{
+					title: 'Github',
+					link: 'https://github.com/Marek-Barela/Raccoon-notes',
+				},
+				{ title: 'Raccon Notes', link: 'https://raccoon-notes.herokuapp.com/' },
+			],
+		},
+		{
+			id: '4',
+			projectTitle: 'Mailbox',
+			projectDescription:
+				'Rozbudowana funkcjonalność aplikacji pocztowej po stronie klienta',
+			link: '/works/mailbox',
+			content: [
+				'Aplikacja stworzona na podobieństwo poczty elektronicznej posiada bogatą ilość operacji takie jak sortowanie, przenoszenie zaznaczonych wiadomości, tworzenie nowych folderów, oznaczanie wiadomości jako przeczytane i nieprzeczytane itd.',
+				'Mailbox został stworzony tylko jako aplikacja frontendowa, przez co nie posiada rozbudowanego backendu. Wszystkie zmiany w aplikacji wracają do ustawień domyślnych po odświeżeniu.',
+			],
+			image: mailbox,
+			externalLinks: [
+				{
+					title: 'Github',
+					link: 'https://github.com/Marek-Barela/Mailbox',
+				},
+				{
+					title: 'Mailbox',
+					link: 'https://agitated-nobel-aff5f8.netlify.app/',
+				},
+			],
+		},
+		{
+			id: '5',
+			projectTitle: 'Currency Converter',
+			projectDescription: 'Przeliczanie i dane historyczne dla 33 różnych walut.',
+			link: '/works/currency-converter',
+			content: [
+				'Aplikacja stworzona jako wersja próbna dla bardzo rozbudowanej konfiguracji projektu',
+				'Konwerter zawiera bardzo bogatą i skalowalną strukturę projektu z wykorzystaniem wielu bibliotek.',
+				'Celem projektu było stworzenie aplikacji z zachowaniem danych po odświerzeniu, możliwość sprawdzenia cen dla określonej waluty oraz kursów historycznych z wykresem.',
+				'Użyte technologie: React, Redux, Redux-Saga, Typescript, Typesafe-Actions, CSS Modules, Ramda, Axios',
+			],
+			image: currencyConverter,
+			externalLinks: [
+				{
+					title: 'Github',
+					link: 'https://github.com/Marek-Barela/Currency-Converter',
+				},
+				{
+					title: 'Currency Converter',
+					link: 'https://hardcore-leakey-952fc7.netlify.com/',
+				},
+			],
+		},
+		{
+			id: '6',
+			projectTitle: 'Slick',
+			projectDescription: 'Szablon aplikacji webowej',
+			link: '/works/slick',
+			content: ['Aplikacja z bardzo ładnym designem stworzona w Reacie.'],
+			image: slick,
+			externalLinks: [
+				{
+					title: 'Github',
+					link: 'https://github.com/Marek-Barela/Slick-React',
+				},
+				{
+					title: 'Slick',
+					link: 'https://determined-mccarthy-d890e0.netlify.app/',
+				},
+			],
+		},
+	],
 };
 
 export const contactContent: ContactContent = {
-  header: "Zbudujmy razem coś wielkiego!",
-  contact: "Kontakt",
-  content: [
-    "Jeśli podobają ci się moje projekty i chciałbyś pracować z osobą asertywną o otwartym umyśle, skontaktuj się ze mną za pomocą poniższych linków.",
-  ],
-  externalLinks: [
-    { title: "marek.barela@wp.pl", link: "mailto:marek.barela@wp.pl" },
-    { title: "Github", link: "https://github.com/Marek-Barela" },
-    {
-      title: "Linkedin",
-      link: "https://www.linkedin.com/in/marek-bare%C5%82a/",
-    },
-  ],
+	header: 'Zbudujmy razem coś wielkiego!',
+	contact: 'Kontakt',
+	content: [
+		'Jeśli podobają ci się moje projekty i chciałbyś pracować z osobą asertywną o otwartym umyśle, skontaktuj się ze mną za pomocą poniższych linków.',
+	],
+	externalLinks: [
+		{ title: 'marek.barela@wp.pl', link: 'mailto:marek.barela@wp.pl' },
+		{ title: 'Github', link: 'https://github.com/Marek-Barela' },
+		{
+			title: 'Linkedin',
+			link: 'https://www.linkedin.com/in/marek-bare%C5%82a/',
+		},
+	],
 };
